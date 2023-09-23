31c044d256ad1270888c2df5a381b5c4ab06a7df
mizan21 <mizanorrahman398@gmail.com>
2023-09-24 00:55:41 +0600

    Deleted part added

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const Blog = ({ blog }) => {
-  const { title, author, description } = blog;
+const Blog = ({ blog, handleBlogDelete }) => {
+  const { _id, title, author, description } = blog;
+
+  const handleDelete = () => {
+    const agree = window.confirm(`Are you sure you want to delete?`);
+    if (agree) {
+      fetch(`http://localhost:5000/blog/${_id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount === 1) {
+            toast.success("Successfully deleted blog.");
+            handleBlogDelete(_id); // Call the onDelete function provided by the parent
+          } else {
+            toast.error("No documents matched the query.");
+          }
+        });
+    }
+  };
+
   return (
     <div className="border">
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold">{title}</h1>
         <div className="flex gap-5">
           <FaEdit />
-          <FaTrash />
+          <button onClick={handleDelete}>
+            <FaTrash />
+          </button>
         </div>
       </div>
       <small>{author}</small>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,20 +2,30 @@ import React, { useEffect, useState } from "react";
 import Blog from "./Blog";
 import BlogPost from "./BlogPost";
 const Blogs = () => {
-  const [blogs, setblogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/blogs")
       .then((res) => res.json())
-      .then((data) => setblogs(data));
+      .then((data) => setBlogs(data));
   }, []);
+
+  const handleBlogDelete = (deletedId) => {
+    const remainingNotes = blogs.filter((blog) => blog._id !== deletedId);
+    setBlogs(remainingNotes);
+  };
+
   return (
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 my-10 container mx-auto">
         <div className="flex justify-center items-center">
           <BlogPost></BlogPost>
         </div>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog}></Blog>
+        {blogs?.map((blog) => (
+          <Blog
+            key={blog?._id}
+            blog={blog}
+            handleBlogDelete={handleBlogDelete}
+          ></Blog>
         ))}
       </div>
     </div>
